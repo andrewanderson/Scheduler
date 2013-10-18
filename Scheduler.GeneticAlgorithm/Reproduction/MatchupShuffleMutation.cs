21534a729776fd0d58172942337f99d460705bc5
Andrew Anderson <andrew.anderson@gmail.com>
2013-10-18 18:19:34 -0400

    Allow multiple week's to shuffle matchups

--- Scheduler.GeneticAlgorithm/Reproduction/MatchupShuffleMutation.cs
+++ Scheduler.GeneticAlgorithm/Reproduction/MatchupShuffleMutation.cs
@@ -8,10 +8,12 @@ using Scheduler.Domain;
 namespace Scheduler.GeneticAlgorithm.Reproduction
 {
     /// <summary>
-    /// Rearrange all of the matchups within a single week of the season
+    /// Rearrange all of the matchups within roughly a tenth of the weeks of the season
     /// </summary>
     public class MatchupShuffleMutation : IMutationAlgorithm
     {
+        private const double shuffleChance = 0.1;
+
         private readonly List<Team> teams;
         private readonly List<GameSlot> slots;
 
@@ -23,15 +25,17 @@ namespace Scheduler.GeneticAlgorithm.Reproduction
 
         public void Mutate(Season season)
         {
-            int weekToShuffle = RandomProvider.Next(season.Weeks.Count);
-
-            var week = season.Weeks[weekToShuffle];
-            week.Games.Clear();
-            
-            var shuffledTeams = FisherYatesShuffleAlgorithm.Shuffle(this.teams);
-            for (int i = 0; i < shuffledTeams.Count / 2; i++)
+            foreach (var week in season.Weeks)
             {
-                week.Games.Add(new Matchup { Home = shuffledTeams[i * 2], Away = shuffledTeams[i * 2 + 1], Slot = this.slots[i] });
+                if (RandomProvider.NextDouble() < shuffleChance)
+                {
+                    week.Games.Clear();
+                    var shuffledTeams = FisherYatesShuffleAlgorithm.Shuffle(this.teams);
+                    for (int i = 0; i < shuffledTeams.Count / 2; i++)
+                    {
+                        week.Games.Add(new Matchup { Home = shuffledTeams[i * 2], Away = shuffledTeams[i * 2 + 1], Slot = this.slots[i] });
+                    }
+                }
             }
         }
     }
